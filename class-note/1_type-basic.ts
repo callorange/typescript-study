dd862eab41084fb21c5fb799392ac4245a012eb9
Yobi <sjy20000@gmail.com>
2025-08-21 05:06:41 +0900

    section 4
    1. 변수 타입
    2. 함수 형태

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,23 @@
+// string
+let str: string = "string";
+
+// number
+let num: number = 109;
+
+// array
+let arr: Array<number> = [1, 2, 3];
+let heroes: Array<string> = ["a"];
+let items: string[] = ["a"];
+
+// tuple
+let address: [string, number] = ["1", 2];
+
+// object
+let obj: object = {};
+let person: { name: string; age: number } = {
+  name: "name",
+  age: 100,
+};
+
+// boolean
+let show: boolean = true;
---
+++ class-note/2_functions.ts
@@ -0,0 +1,12 @@
+// function
+
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+sum(10, 20);
+
+// optional parameter
+function log(a: string, b?: string) {}
+log("1");
+log("1", "2");
