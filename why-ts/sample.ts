7a4208e2c27797f6127ce79e85d7ba06107ec157
Yobi <sjy20000@gmail.com>
2025-08-21 04:13:03 +0900

    section 1~2
    why typescript

---
+++ why-ts/sample.ts
@@ -0,0 +1,6 @@
+function add(a: number, b: number) {
+  return a + b;
+}
+
+var addResult = add(10, 20);
+addResult.toLocaleString();
---
+++ why-ts/smaple.js
@@ -0,0 +1,12 @@
+//@ts-check
+
+/**
+ * @param {number} a
+ * @param {number} b
+ */
+function sum(a, b) {
+  return a + b;
+}
+
+var sumResult = sum(10, 20);
+sumResult.toLocaleString();
